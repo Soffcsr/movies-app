797bc3acbf5d72a424ab735e213ae7d173efcf71
Soffcsr <soff.csr@gmail.com>
2023-12-27 23:07:09 -0300

    movies slice - react redux toolkit

--- src/redux/reducers/moviesSlice.js
+++ src/redux/reducers/moviesSlice.js
@@ -1,17 +1,45 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchPopularMovies = createAsyncThunk('movies/fetchPopularMovies', async () => {
+    const response = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer 24983e85f00f86fe5580319212bd1224'
+          }
+    })
+    const jsonData = await response.json();
+    return jsonData;
+})
 
 export const moviesSlice = createSlice({
     name: 'movies',
     initialState: {
-        movies: []
+        movies: [],
+        loading: false,
+        error: false,
     },
     reducers: (state) => {
         add: (state, action) => {
            state.movies =  action.payload
         }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchPopularMovies.pending,  (state) => {
+                state.loading = true;
+            })
+            .addCase(fetchPopularMovies.fulfilled, (state, action) => {
+                state.loading = false;
+                state.data = action.payload
+            })
+            .addCase(fetchPopularMovies.rejected, (state, action) => {
+                state.loading = false;
+                state.error = true;
+            })
     }
 })
 
-export const { add } = moviesSlice.actions
+//export const { add } = moviesSlice.actions
 
 export default moviesSlice.reducer
